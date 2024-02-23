1fb05ed401156c18d737ba4c02f4abe13a220d76
William Hicks <wch47909@email.vccs.edu>
2024-02-23 13:26:19 -0500

    intLinkedList developed with splitEvensOddsList function; added client program

---
+++ client.cpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "intLinkedList.h"
+
+int main()
+{
+	intLinkedList numsList; //Original list of numbers
+	intLinkedList evens; //Split sublist of only even numbers from original list
+	intLinkedList odds; //Split sublist of only odd numbers from original list
+	linkedListIterator<int> listPtr; //Pointer of original list
+	linkedListIterator<int> evenPtr; //Pointer of evens list
+	linkedListIterator<int> oddPtr; //Pointer of odds list
+
+	int input = 0; //Input variable for list entries
+	
+	cout << "Please input integers for list. Use -999 to end the list." << endl;
+	//Following while loop constructs original list via user input
+	while (input != -999)
+	{
+		cin >> input;
+		if (input != -999)
+			numsList.insertLast(input);
+	}
+
+	//Prints original list
+	cout << endl << "Original: ";
+	for (listPtr = numsList.begin(); listPtr != numsList.end(); ++listPtr)
+		cout << *listPtr << " ";
+	
+	//Splits the list into evens and odds lists
+	cout << endl << endl << "Splitting the list... " << endl;
+	numsList.splitEvensOddsList(evens, odds);
+
+	//Prints the evens list
+	cout << endl << "Evens: ";
+	for (evenPtr = evens.begin(); evenPtr != evens.end(); ++evenPtr)
+		cout << *evenPtr << " ";
+
+	//Prints the odds list
+	cout << endl << "Odds: ";
+	for (oddPtr = odds.begin(); oddPtr != odds.end(); ++oddPtr)
+		cout << *oddPtr << " ";
+
+	//Prints the original list again, ensuring it is empty
+	cout << endl << "Original (After Split, making sure it is empty): ";
+	for (listPtr = numsList.begin(); listPtr != numsList.end(); ++listPtr)
+		cout << *listPtr << " ";
+	
+	cout << endl;
+
+
+
+	return 0;
+}
--- intLinkedList.h
+++ intLinkedList.h
@@ -5,10 +5,32 @@ class intLinkedList: public unorderedLinkedList<int>
 {
 	public:
 		void splitEvensOddsList(intLinkedList& evensList, intLinkedList& oddsList);
+		// Precond: A pre-existing linked list of integers is formed
+		// Postcond: Said list is 'split' ino an evens and odds list, before the original list is destroyed
+		//			L> Evens: Contains only the original list's even numbers
+		//			L> Odds: Contains only the original list's odd numbers
 
 };
 
 void intLinkedList::splitEvensOddsList(intLinkedList& evensList, intLinkedList& oddsList)
 {
+	nodeType<int> *current; //Creates a node pointer
 
+	current = this->first; //Sets the pointer to the first entry
+
+	while (current != nullptr)
+	{
+		if (current->info % 2 == 0)
+		{
+			evensList.insertLast(current->info); //Adds the current node as the next entry in the evens entry
+			current = current->link; //Moves the pointer to the next entry
+		}
+		else
+		{
+			oddsList.insertLast(current->info); //Adds the current node as the next entry in the odds entry
+			current = current->link; //Moves the pointer to the next entry
+		}
+	}
+
+	destroyList(); //Destroys the original list
 }
