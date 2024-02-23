106392ed176b3c0b8ea7c5955b551a2de10e3434
WilliamH1cks <wch47909@email.vccs.edu>
2024-02-23 10:34:12 -0500

    Add project files.

---
+++ intLinkedList.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "unorderedLinkedList.h"
+
+class intLinkedList: public unorderedLinkedList<int>
+{
+	public:
+		void splitEvensOddsList(intLinkedList& evensList, intLinkedList& oddsList);
+
+};
+
+void intLinkedList::splitEvensOddsList(intLinkedList& evensList, intLinkedList& oddsList)
+{
+
+}
